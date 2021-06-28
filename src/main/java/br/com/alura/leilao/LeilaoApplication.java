9b44e283d0c319a2e1f4196bef17bbdeaaedb2db
apontes77 <alexandrepontesgyn@gmail.com>
2021-06-27 23:08:28 -0300

    commit de teste para build automatico

--- src/main/java/br/com/alura/leilao/LeilaoApplication.java
+++ src/main/java/br/com/alura/leilao/LeilaoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeilaoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("oi!!");
 		SpringApplication.run(LeilaoApplication.class, args);
 	}
 
